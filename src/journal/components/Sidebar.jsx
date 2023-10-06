4fe06ad07ebb70506466a6980990fc22a6d8d666
LostoVayne <moises.barrera.dev@gmail.com>
2023-10-06 02:25:54 -0300

    agregado sidebar

---
+++ src/journal/components/Sidebar.jsx
@@ -0,0 +1,55 @@
+import { TurnedInNot } from "@mui/icons-material";
+import {
+    Box,
+    Divider,
+    Drawer,
+    Grid,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
+    Toolbar,
+    Typography,
+} from "@mui/material";
+
+// eslint-disable-next-line react/prop-types
+export const Sidebar = ({ drawerWidth }) => {
+    return (
+        <Box component={"nav"} sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}>
+            {/* <Drawer variant="temporary" open={false}></Drawer> */}
+            <Drawer
+                variant="permanent"
+                open
+                sx={{
+                    display: { xs: "block" },
+                    "& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth },
+                }}
+            >
+                <Toolbar>
+                    <Typography variant="h6" noWrap component="div">
+                        Epsaind
+                    </Typography>
+                </Toolbar>
+
+                <Divider />
+
+                <List>
+                    {["Enero", "Febrero", "Marzo", "Abril"].map((text) => (
+                        <ListItem key={text} disablePadding>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <TurnedInNot />
+                                </ListItemIcon>
+                                <Grid container>
+                                    <ListItemText primary={text} />
+                                    <ListItemText secondary={"Lorem ipsum dolor sit amet"} />
+                                </Grid>
+                            </ListItemButton>
+                        </ListItem>
+                    ))}
+                </List>
+            </Drawer>
+        </Box>
+    );
+};
--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,17 +1,18 @@
-import { Box } from '@mui/material';
-import { Navbar } from '../components';
+import { Box, Toolbar } from "@mui/material";
+import { Navbar, Sidebar } from "../components";
 
-const drawerWidth = 240;
+const drawerWidth = 280;
 
 // eslint-disable-next-line react/prop-types
 export const JournalLayout = ({ children }) => {
     return (
-        <Box sx={{ display: 'flex' }}>
+        <Box sx={{ display: "flex" }}>
             <Navbar drawerWidth={drawerWidth} />
             {/* Sidebar */}
+            <Sidebar drawerWidth={drawerWidth} />
 
-            <Box component='main' sx={{ flexGrow: 1, p: 3 }}>
-                {/* Toolbar */}
+            <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+                <Toolbar />
                 {children}
             </Box>
         </Box>

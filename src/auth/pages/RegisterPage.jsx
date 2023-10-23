c010872c836e10d5bcb04a304a9333e89ec1afd5
LostoVayne <moises.barrera.dev@gmail.com>
2023-10-22 22:56:38 -0300

    formdata

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,46 +1,99 @@
-import { Button, Grid, Link, TextField, Typography } from '@mui/material';
-import { Link as RouterLink } from 'react-router-dom';
-import { AuthLayout } from '../layout/AuthLayout';
+import { Button, Grid, Link, TextField, Typography } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import { AuthLayout } from "../layout/AuthLayout";
+import { useForm } from "../../hooks";
+
+const formData = {
+    email: "epsaind@gmail.com",
+    password: "123456",
+    displayName: "Franco",
+};
+
+const formValidations = {
+    email: [(value) => value.includes("@"), "El correo debe de tener una @"],
+    password: [(value) => value.length >= 6, "El password debe de tener mas de 6 caracteres"],
+    displayName: [(value) => value.length >= 1, "El nombre es obligatorio"],
+};
 
 export const RegisterPage = () => {
+    const {
+        formState,
+        displayName,
+        email,
+        password,
+        onInputChange,
+        isFormValid,
+        displayNameValid,
+        emailValid,
+        passwordValid,
+    } = useForm(formData, formValidations);
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        // console.log(formState);
+    };
+
     return (
-        <AuthLayout title='Register'>
-            <form action=''>
+        <AuthLayout title="Register">
+            <form action="" onSubmit={onSubmit}>
                 <Grid container>
                     <Grid item xs={12} sx={{ mb: 2 }}>
-                        <TextField label='Name' type='text' placeholder='Your Name' variant='outlined' fullWidth />
+                        <TextField
+                            label="Name"
+                            type="text"
+                            placeholder="Your Name"
+                            variant="outlined"
+                            fullWidth
+                            name="displayName"
+                            value={displayName}
+                            onChange={onInputChange}
+                            error={!displayNameValid}
+                            helperText={displayNameValid}
+                        />
                     </Grid>
 
                     <Grid item xs={12} sx={{ mb: 2 }}>
                         <TextField
-                            label='Email'
-                            type='email'
-                            placeholder='correo@example.com'
-                            variant='outlined'
+                            label="Email"
+                            type="email"
+                            placeholder="correo@example.com"
+                            variant="outlined"
                             fullWidth
+                            name="email"
+                            value={email}
+                            onChange={onInputChange}
                         />
                     </Grid>
 
                     <Grid item xs={12} sx={{ mb: 1 }}>
                         <TextField
-                            label='Password'
-                            type='password'
-                            placeholder='********'
-                            variant='outlined'
+                            label="Password"
+                            type="password"
+                            placeholder="********"
+                            variant="outlined"
                             fullWidth
+                            name="password"
+                            value={password}
+                            onChange={onInputChange}
                         />
                     </Grid>
                     <Grid container spacing={2} sx={{ my: 1 }}>
                         <Grid item xs={12}>
-                            <Button variant='contained' color='primary' fullWidth sx={{ py: 1 }}>
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                fullWidth
+                                sx={{ py: 1 }}
+                                type="submit"
+                            >
                                 Create Account
                             </Button>
                         </Grid>
                     </Grid>
 
-                    <Grid container direction={'row'} justifyContent='end' sx={{ mt: 1 }}>
+                    <Grid container direction={"row"} justifyContent="end" sx={{ mt: 1 }}>
                         <Typography>¿Already have an account? </Typography>
-                        <Link component={RouterLink} color='inherit' to='/auth/login'>
+                        <Link component={RouterLink} color="inherit" to="/auth/login">
                             Login
                         </Link>
                     </Grid>

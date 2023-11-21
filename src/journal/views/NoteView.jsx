1768878d36139299b4b1d48703d0760a1de6c5fe
LostoVayne <moises.barrera.dev@gmail.com>
2023-11-20 23:51:11 -0300

    test cloudinary subir y eliminar imagen

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,14 +1,18 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { SaveOutlined, UploadOutlined } from "@mui/icons-material";
+import { DeleteOutline, SaveOutlined, UploadOutlined } from "@mui/icons-material";
 import { Button, Grid, IconButton, TextField, Typography } from "@mui/material";
 import { useEffect, useMemo, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Swal from "sweetalert2";
 import "sweetalert2/dist/sweetalert2.css";
-import { setActiveNotes, startDeletingNote, startSaveNote, startUploadingFiles } from "../../store/journal";
+import {
+    setActiveNotes,
+    startDeletingNote,
+    startSaveNote,
+    startUploadingFiles,
+} from "../../store/journal";
 import { ImageGallery } from "../components";
 import { useForm } from "./../../hooks/useForm";
-import { DeleteOutline } from "@mui/icons-material";
 
 export const NoteView = () => {
     const { active: note, messageSaved, isSaving } = useSelector((state) => state.journal);
@@ -45,7 +49,13 @@ export const NoteView = () => {
     };
 
     return (
-        <Grid container direction="row" justifyContent={"space-between"} alignItems={"center"} sx={{ mb: 1 }}>
+        <Grid
+            container
+            direction="row"
+            justifyContent={"space-between"}
+            alignItems={"center"}
+            sx={{ mb: 1 }}
+        >
             <Grid item>
                 <Typography fontSize={39} fontWeight={"light"}>
                     {dateString}
@@ -61,7 +71,11 @@ export const NoteView = () => {
                     ref={fileInputRef}
                 />
 
-                <IconButton disabled={isSaving} onClick={() => fileInputRef.current.click()} color="primary">
+                <IconButton
+                    disabled={isSaving}
+                    onClick={() => fileInputRef.current.click()}
+                    color="primary"
+                >
                     <UploadOutlined />
                 </IconButton>
 
